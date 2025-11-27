3cac683ee3b19a86c401b8d14c59a61d0f2a0f20
mzu-2410z <mzuthedev@gmail.com>
2025-11-27 11:59:13 +0500

    Cart model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="cart",
+        null=True,
+        blank=True
+    )
+    session_key = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def total_items(self):
+        return sum(item.quantity for item in self.items.all())
+
+    def total_price(self):
+        return sum(item.subtotal() for item in self.items.all())
+
+    def __str__(self):
+        if self.user:
+            return f"Cart ({self.user.email})"
+        return f"Cart (Session: {self.session_key})"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name="items"
+    )
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="cart_items"
+    )
+    quantity = models.PositiveIntegerField(default=1)
+
+    def subtotal(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.title}"
